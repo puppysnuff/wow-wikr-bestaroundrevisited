d2b1e506cbd43b10ea2346498a5240976848ce1b
puppysnuff <puppysnuff@gmail.com>
2024-06-26 20:00:23 -0700

    fixed config bugs

--- BestAroundRevisited.lua
+++ BestAroundRevisited.lua
@@ -1,4 +1,5 @@
--- register the addon command
+-- data storage
+
 if not BestAroundRevisitedDB then 
 	BestAroundRevisitedDB = {
 		config = {
@@ -8,12 +9,21 @@ if not BestAroundRevisitedDB then
 	}
 end
 
--- BestAround = {
--- 	config = {
--- 		achievements = true,
--- 		levels = true,
--- 	}
--- }
+
+-- assets
+
+local WOWsounds = {}
+WOWsounds[#WOWsounds+1] = 'Interface\\AddOns\\BestAroundRevisited\\assets\\bestaround.mp3'
+local soundLength = #WOWsounds
+print('BestAroundReloaded: loaded ' .. soundLength .. ' looting sounds.')
+
+
+-- main frame
+
+local mainFrame = CreateFrame("Frame", "BestAroundRevisitedFrame", UIParent, "BasicFrameTemplateWithInset")
+
+
+-- functionality
 
 local function updateSettings(setting, value)
 	if BestAroundRevisitedDB.config[setting] == nil then
@@ -34,59 +44,45 @@ local function updateSettings(setting, value)
 	print("BestAroundRevisited: " .. setting .. " is now " .. (BestAroundRevisitedDB.config[setting] and "on" or "off"))
 end
 
-local SlashCommand = "/bestaround"
-local SlashCommandList["BESTAROUNDREVISITED"] = function(msg)
-	-- parse the setting
-	local command, value = msg:match("^(%S*)%s*(.-)$")
 
-	-- handle command
-	if command == "get" then
-		print("BestAroundRevisited: Settings")
-		for k, v in pairs(BestAround.config) do
-			print(k .. ": " .. (v and "on" or "off"))
+-- slash command
+
+SLASH_BESTAROUND1 = "/bar"
+SLASH_BESTAROUND2 = "/bestaround"
+SlashCmdList["BESTAROUND"] = function(msg)
+		-- parse the setting
+		local command, value = msg:match("^(%S*)%s*(.-)$")
+
+		-- handle command
+		if command == "get" then
+			print("BestAroundRevisited: Settings")
+			for k, v in pairs(BestAroundRevisitedDB.config) do
+				print(k .. ": " .. (v and "on" or "off"))
+			end
+		elseif BestAroundRevisitedDB.config[command] ~= nil then
+			updateSettings(command, value)
+		else
+			-- Print usage information
+			print("BestAround usage:")
+			print("/bestaround <achievements|levels> <on|off|toggle>")
+			print("/bestaround get")
 		end
-	elseif BestAround.config[command] ~= nil then
-		updateSettings(command, value)
-	else
-		-- Print usage information
-		print("BestAround usage:")
-		print("/bestaround <setting> <on|off|toggle>")
-		print("/bestaround get")
-		print("Available settings: achievements, levels")
-	end
-
-	updateSettings(setting, value)
 end
 
+-- event listener
 
-local WOWsounds = {}
-WOWsounds[#WOWsounds+1] = 'Interface\\AddOns\\BestAroundRevisited\\assets\\bestaround.mp3'
+local eventListenerFrame = CreateFrame("Frame", "BestAroundRevisitedListenerFrame", UIParent)
 
-local soundLength = #WOWsounds
-
-local levelFrame = CreateFrame("Frame")
-
-levelFrame:RegisterEvent("PLAYER_LEVEL_UP")
-levelFrame:RegisterEvent("ACHIEVEMENT_EARNED")
-
-
-print('BestAroundReloaded loaded ' .. soundLength .. ' looting sounds.')
-
-
-levelFrame:SetScript("OnEvent",function(self, event, ...)
-	print("Debugging: event is " .. event)
-	print("Debugging: achievements is " .. BestAroundRevisitedDB.config.achievements)
-	print("Debugging: levels is " .. BestAroundRevisitedDB.config.levels)
-
-	local g = math.random(soundLength)
-
+local function eventHandler(self, event, ...)
 	if event == "PLAYER_LEVEL_UP" and BestAroundRevisitedDB.config.levels then
-		print("Debugging: you have leveled up!")
-		PlaySoundFile(WOWsounds[g], "Master")
+		PlaySoundFile(WOWsounds[math.random(soundLength)], "Master")
+	elseif event == "ACHIEVEMENT_EARNED" and BestAroundRevisitedDB.config.achievements then
+		PlaySoundFile(WOWsounds[math.random(soundLength)], "Master")
 	end
+end
 
-	if event == "ACHIEVEMENT_EARNED" and BestAroundRevisitedDB.config.achievements then
-		print("Debugging: you have earned an achievement!")
-		PlaySoundFile(WOWsounds[g], "Master")
-	end
-end)
+eventListenerFrame:SetScript("OnEvent", eventHandler)
+eventListenerFrame:RegisterEvent("PLAYER_LEVEL_UP")
+eventListenerFrame:RegisterEvent("ACHIEVEMENT_EARNED")
+
+table.insert(UISpecialFrames, "BestAroundRevisitedFrame")
